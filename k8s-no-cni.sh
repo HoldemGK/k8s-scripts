ef77e7b2ca1c88975020a54f4b69e8fbf167d568
HoldemGK <atos.holdemgk@gmail.com>
2024-08-16 21:01:55 +0200

    updated apt repo in k8s-no-cni.sh

--- k8s-no-cni.sh
+++ k8s-no-cni.sh
@@ -50,7 +50,8 @@ sudo systemctl restart containerd
 
 # Initialize a control plane node
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 sudo apt-get update
 sudo apt-get install -y kubeadm
 sudo swapoff -a
